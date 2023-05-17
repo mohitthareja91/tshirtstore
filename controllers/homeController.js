dd4cf510a908d8ae9831834ee0b2fd7f797b5fb9
mohit <mohit.thareja91@gmail.com>
2023-05-17 17:31:46 +0530

    commit on master brach

---
+++ controllers/homeController.js
@@ -0,0 +1,19 @@
+const BigPromise = require("../middlewares/bigPromise");
+
+exports.home = BigPromise(async (req, res) => {
+  res.status(200).json({
+    success: true,
+    greeting: "Hello from API",
+  });
+});
+
+exports.homeDummy = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      greeting: "this is another dummy route",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
